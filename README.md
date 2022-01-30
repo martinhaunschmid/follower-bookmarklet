23b0bb9243a61f9e52359194d6182bc39e763d7f
Martin <martin@haunschmid.tech>
2022-01-30 18:34:42 +0100

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Installation
+
+1. Create a new Bookmark
+2. As Bookmark URL add `javascript:` and then the contents of `bookmarklet.js`
+
+# Usage
+
+1. navigate to `linkedin.com/feed/followers`
+2. scroll af far as you can
+3. run the bookmarklet
---
+++ bookmarklet.js
@@ -0,0 +1,28 @@
+(function(){
+	let followers = document.querySelectorAll("section.follows-recommendation-card__content");
+  let result = [];
+  followers.forEach(f => {
+		let linkElem =  f.querySelector(".follows-recommendation-card__avatar-link");
+    let link = linkElem.href;
+		let image = linkElem.querySelector("img").src;
+		let name = f.querySelector(".follows-recommendation-card__name").innerText;
+		let subheadline = f.querySelector(".follows-recommendation-card__headline").innerText;
+    let details = f.querySelector(".follows-recommendation-card__subtext").innerText;
+    result.push({
+      name: name,
+      image: image,
+      link: link,
+      subheadline: subheadline,
+      details: details
+    });
+  });
+  scraperesult=result;
+  tab = window.open("about:blank", "_blank");
+  downloadLink = tab.document.createElement('a');
+  downloadLink.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(scraperesult)));
+  downloadLink.setAttribute('download', 'followers.json');
+  downloadLink.style.display = 'none';
+  tab.document.body.appendChild(downloadLink);
+  downloadLink.click();
+  tab.document.body.removeChild(downloadLink);
+})();
