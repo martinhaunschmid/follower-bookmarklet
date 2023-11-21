f43ee3e5e1cdcdb7a57ab0ea8dd02a439d1c3884
Martin <martin@haunschmid.tech>
2023-11-21 10:08:53 +0100

    adapt to new UI

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,13 +1,13 @@
 (function(){
-	let followers = document.querySelectorAll("section.follows-recommendation-card__content");
+  let followers = document.querySelectorAll("section.follows-recommendation-card__content");
   let result = [];
   followers.forEach(f => {
-		let linkElem =  f.querySelector(".follows-recommendation-card__avatar-link");
+    let linkElem =  f.querySelector(".follows-recommendation-card__avatar-link");
     let link = linkElem.href;
-		let image = linkElem.querySelector("img").src;
-		let name = f.querySelector(".follows-recommendation-card__name").innerText;
-		let subheadline = f.querySelector(".follows-recommendation-card__headline").innerText;
-    let details = f.querySelector(".follows-recommendation-card__subtext") ? f.querySelector(".follows-recommendation-card__subtext").innterText : "";
+    let image = linkElem.querySelector("img").src;
+    let name = f.querySelector(".follows-recommendation-card__name").innerText;
+    let subheadline = f.querySelector(".follows-recommendation-card__headline").innerText;
+    let details = f.querySelector(".follows-recommendation-card__subtext") ? f.querySelector(".follows-recommendation-card__subtext").innerText : "";
     result.push({
       name: name,
       image: image,
